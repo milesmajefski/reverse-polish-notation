0c1a954d2f6ffcca24d1638943dfdf3e2f0c9f50
Miles <milesmajefski@gmail.com>
2020-01-22 09:10:15 -0600

    - added tests for the shared parsing code

---
+++ tests/test_rpn_shared.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+sys.path.append('./src')
+from rpn_shared import parse_float
+
+
+class TestRPNShared(unittest.TestCase):
+
+    def test_easy_parse(self):
+        result = parse_float('2.0 3 +')
+        self.assertListEqual(result['parsed'], [2, 3, '+'])
+        self.assertIsNone(result['error_msg'])
+    
+    def test_unknown_char(self):
+        result = parse_float('2 3 $')
+        self.assertListEqual(result['parsed'], [2, 3])
+        self.assertIsNotNone(result['error_msg'])
+
+    def test_multiple_unknown_chars(self):
+        result = parse_float('2 3 $$')
+        self.assertListEqual(result['parsed'], [2, 3])
+        self.assertIsNotNone(result['error_msg'])
+    
+    def test_complete_shit_input(self):
+        result = parse_float('9867g96ggu9yg7g76g87 t787g668t78 8778')
+        self.assertListEqual(result['parsed'], [])
+        self.assertIsNotNone(result['error_msg'])
+
+
+if __name__ == '__main__':
+    unittest.main()
