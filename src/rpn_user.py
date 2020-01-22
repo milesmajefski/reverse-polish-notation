a93110c4edc7f83426226c20c5cb428fd2fa5160
Miles <milesmajefski@gmail.com>
2020-01-21 20:06:24 -0600

    - refactor startup and implement pipe support

--- src/rpn_user.py
+++ src/rpn_user.py
@@ -10,13 +10,14 @@ class RPN_cli:
     This class implements a cmd line interface for the RPN_calculator class.
     This scripts accepts cmd line args or starts an interactive calculator mode.
     """
+
     def __init__(self):
         self.please_continue = True
         self.operation_stack = deque()
         self.user_input = None
         self.calc = rpn.RPN_calculator()
 
-    def main(self):
+    def interactive_mode(self):
         print('\nInteractive Mode Reverse Polish Notation Calculator')
         print('-' * 20)
         print('type "q" to quit, "c" to clear the stack')
@@ -60,14 +61,27 @@ class RPN_cli:
             self.user_input = user_input
 
 
+def run_headless(input_string):
+    cli = RPN_cli()
+    cli.user_input = input_string
+    cli.parse_and_evaluate()
+    print(list(cli.operation_stack))
+
+
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if not args:
-        # if no cmd line args are given, then go into interactive mode
-        RPN_cli().main()
-    else:
-        # otherwise process the args and respond on stdout
-        cli = RPN_cli()
-        cli.user_input = ' '.join(args)
-        cli.parse_and_evaluate()
-        print(list(cli.operation_stack))
+    args = sys.argv[1:]    
+
+    if len(args) == 1 and args[0] == '-':
+        # we are doing pipe mode
+        pipe_data = sys.stdin.readlines()
+        for line in pipe_data:
+            run_headless(line)
+        exit()
+
+    if args:
+        # we are doing headless using the args
+        run_headless(' '.join(args))
+        exit()
+
+    # we are doing interactive mode
+    RPN_cli().interactive_mode()
